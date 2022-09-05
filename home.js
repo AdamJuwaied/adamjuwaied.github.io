0d3cece8d6a07228800a1d0ff8cbbe8026480e4e
Adam Juwaied <juwaieda19@students.ecu.edu>
2022-09-05 02:06:59 -0400

    Update on home page

---
+++ home.js
@@ -0,0 +1,7 @@
+let animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets2.lottiefiles.com/temp_editor_files/lf30_editor_l0xh1ey6.json'
+});
